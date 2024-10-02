6706d12f676e7347626c3d0434f599ce785ce435
Pavel Isaenko <82381868+Poke0647@users.noreply.github.com>
2024-10-03 01:25:14 +1000

    Update ServiceScheduler.java

--- src/main/java/com/pavelisaenko/exchangerateservice/ServiceScheduler.java
+++ src/main/java/com/pavelisaenko/exchangerateservice/ServiceScheduler.java
@@ -10,18 +10,11 @@ public class ServiceScheduler {
 
 //    private static final String CRON = "0 * * * * *";
 
-    private final Parser parser;
-
-    private final Exporter exporter;
-
-    public ServiceScheduler(){
-        this.parser = new RSHBParser();
-        this.exporter = new CSVExporter();
-    }
-
 
     @Scheduled(fixedRate = 21600000)    // cron = CRON
     public void parseAndExport(){
+        Parser parser = new RSHBParser();
+        Exporter exporter = new CSVExporter();
         exporter.exportItems(parser.parse());
         System.out.println("Hello");
     }
