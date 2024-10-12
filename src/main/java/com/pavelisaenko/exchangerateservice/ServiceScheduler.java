3e66ed4007780578de2b2f037ba5652d0af32af9
Poke0647 <82381868+Poke0647@users.noreply.github.com>
2024-10-12 21:24:15 +1000

    added Bank Primorye scheduler

--- src/main/java/com/pavelisaenko/exchangerateservice/ServiceScheduler.java
+++ src/main/java/com/pavelisaenko/exchangerateservice/ServiceScheduler.java
@@ -1,22 +1,41 @@
 package com.pavelisaenko.exchangerateservice;
 
 // import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-//@Slf4j
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+
+@Slf4j
 @Service
 public class ServiceScheduler {
 
 //    private static final String CRON = "0 * * * * *";
 
 
-    @Scheduled(fixedRate = 21600000)    // cron = CRON
-    public void parseAndExport(){
+    @Scheduled(fixedRate = 10800000)    // cron = CRON
+    public void parseAndExportRSHBToCSV(){
         Parser parser = new RSHBParser();
         Exporter exporter = new CSVExporter();
         exporter.exportItems(parser.parse());
-        System.out.println("Hello");
+
+        DateTimeFormatter dtf = new DateTimeFormatterBuilder().appendPattern("d.M.y H:m:s").toFormatter();
+        String dateTime = LocalDateTime.now().format(dtf);
+        System.out.printf("[%s] RSHB => CSV", dateTime);
+    }
+
+    @Scheduled(fixedRate = 1000)
+    public void parseAndExportBPToCSV(){
+        Parser parser = new BPParser();
+        Exporter exporter = new CSVExporter();
+        exporter.exportItems(parser.parse());
+
+        DateTimeFormatter dtf = new DateTimeFormatterBuilder().appendPattern("d.M.y H:m:s").toFormatter();
+        String dateTime = LocalDateTime.now().format(dtf);
+        System.out.printf("[%s] BP => CSV", dateTime);
     }
 
 }
